eea4b3fac491c3f3e7a641859dd2de16d0602492
tludwig <business@tludwig.dev>
2026-08-29 19:22:36 +0200

    chore: update packages, maybe it works great?

--- dist/assets/lazy-BwDVr7ZB.js
+++
@@ -1,1 +0,0 @@
-import{Gr as e,Jr as t,fr as n,m as r}from"./dimensions-CIWMyuP3.js";var i=r({eager:Boolean},`lazy`);function a(r,i){let a=e(!1),o=t(()=>a.value||r.eager||i.value);n(i,()=>a.value=!0);function s(){r.eager||(a.value=!1)}return{isBooted:a,hasContent:o,onAfterLeave:s}}export{a as n,i as t};
